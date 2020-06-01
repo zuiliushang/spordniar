635cc0d53d7ac7d590e1c22834f4f28401f91fe8
zuiliushang <383688501@qq.com>
2020-06-01 12:19:17 +0800

    jenkin file

--- spordniar-app/src/main/java/com/spordniar/spordniar/SpordniarAppApplication.java
+++ spordniar-app/src/main/java/com/spordniar/spordniar/SpordniarAppApplication.java
@@ -2,12 +2,20 @@ package com.spordniar.spordniar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpordniarAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpordniarAppApplication.class, args);
 	}
 
+	@GetMapping("greeting")
+	public String greeting() {
+		return "hello spordniar";
+	}
+	
 }
